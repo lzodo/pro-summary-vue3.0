d2e83fcc6b61a4d09dc842063b113ea1f0c82f3a
lenovo-kali <869664233@qq.com>
2023-02-14 21:55:40 +0800

    2023-02-14 21:55提交上传

---
+++ dist/js/973.2eeb2380.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpro_summary_vue3_0"]=self["webpackChunkpro_summary_vue3_0"]||[]).push([[973],{8973:function(e,u,a){a.r(u),a.d(u,{default:function(){return v}});var s=a(3396),r=a(7139),t=a(4870);const n={class:"r-page page"};var _={__name:"vueuse",setup(e){let u=(0,t.qj)({a:1});return(e,a)=>((0,s.wg)(),(0,s.iD)("div",n," vueuse"+(0,r.zw)((0,t.SU)(u).a),1))}},c=a(89);const p=(0,c.Z)(_,[["__scopeId","data-v-797f7af5"]]);var v=p}}]);
+//# sourceMappingURL=973.2eeb2380.js.map
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // "proxyUrl": "http://114.115.212.129:8778", // 跨域代理
-    "proxyUrl": "http://localhost:8778",
+    "proxyUrl": "http://114.115.212.129:8778", // 跨域代理
+    // "proxyUrl": "http://localhost:8778",
     "baseUrl": process.env.VUE_APP_BASE_API, // 文件地址
     "socketUrl": process.env.VUE_APP_SOCKET_API,
 }
